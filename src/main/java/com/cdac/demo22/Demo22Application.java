d4111f55b4a36a4cd07f3a77e10b71ab27e215a4
varshadabade <varshadabade@gmail.com>
2019-06-14 16:49:17 +0530

    projecte commit

---
+++ src/main/java/com/cdac/demo22/Demo22Application.java
@@ -0,0 +1,13 @@
+package com.cdac.demo22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdac/demo22/Demo22ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cdac.demo22;
+
+import org.testng.annotations.Test;
+
+public class Demo22ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
